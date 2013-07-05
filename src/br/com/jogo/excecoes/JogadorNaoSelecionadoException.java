dc6e7a220c7387f78ddb7bbeb8aaa93ed47acf54
Lilian <lilian.thais@dce.ufpb.br>
2013-07-05 16:06:38 -0300

    Alterando documentação e comentários da classe JogadorNaoSelecionadoException

--- src/br/com/jogo/excecoes/JogadorNaoSelecionadoException.java
+++ src/br/com/jogo/excecoes/JogadorNaoSelecionadoException.java
@@ -3,17 +3,21 @@ package br.com.jogo.excecoes;
 public class JogadorNaoSelecionadoException extends Exception {
 	private String erro;
 
-	// Construtor padrao - inicializa variavel de instancia para o desconhecido
+	// Construtor padrao - inicializa a variável de instância para o desconhecido.
+	
 	/**
 	 * Construtor padrão da Exceção que altera a mensagem de erro para "unknown"
 	 */
 	public JogadorNaoSelecionadoException() {
-		super(); // chamar o construtor da superclasse
+		super(); 			// chamar o construtor da superclasse
 		erro = "unknown";
 	}
 
-	// Construtor recebe algum tipo de mensagem que e salvo em uma Variavel de
-	// instancia
+	/*
+	 * Construtor recebe algum tipo de mensagem que é salvo em uma variável de
+	 * instância.
+	 */
+
 	/**
 	 * Construtor da Exceção que recebe uma mensagem de erro
 	 * 
@@ -22,13 +26,14 @@ public class JogadorNaoSelecionadoException extends Exception {
 	 */
 	public JogadorNaoSelecionadoException(String err) {
 		super(err); // chamar o construtor da superclasse
-		erro = err; // salva messagem
+		erro = err; // salva menssagem
 	}
 
 	/*
-	 * metodo publico, pode ser chamado pelo coletor de excecao. Ele retorna a
-	 * mensagem de erro
+	 * Método público, pode ser chamado pelo coletor de excecão. Ele retorna a
+	 * mensagem de erro.
 	 */
+	
 	/**
 	 * Descrição do método
 	 * 
