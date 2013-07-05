544c38a5b240c0c213ab7a4e37a0b6037c45625e
Lilian <lilian.thais@dce.ufpb.br>
2013-07-05 15:36:43 -0300

    Commit classe Jogador

---
+++ src/br/com/jogo/entidades/Jogador.java
@@ -0,0 +1,66 @@
+package br.com.jogo.entidades;
+
+public class Jogador {
+	// atributos
+	private String nome;
+	private int pontuacao;
+	private int raking;
+
+	// construtor
+	public Jogador() {
+		this("", 0, 0);
+	}
+
+	// construtor
+	/**
+	 * Construtor da classe jogador que recebe nome e pontuação
+	 * 
+	 * @param nome
+	 *            recebe o nome do jogador
+	 * @param pontuacao
+	 *            recebe a pontuação do jogador
+	 * @param ranking
+	 *            recebe o ranking do jogador
+	 */
+	public Jogador(String nome, int pontuacao, int raking) {
+		this.nome = nome;
+		this.pontuacao = pontuacao;
+		this.raking = raking;
+	}
+
+	// get: pegar
+	public String getNome() {
+		return this.nome;
+	}
+
+	// set: modificar
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getPontuacao() {
+		return this.pontuacao;
+	}
+
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+
+	public int getRaking() {
+		return this.raking;
+	}
+
+	public void setRaking(int raking) {
+		this.raking = raking;
+	}
+
+	/**
+	 * Serve para retornar informações da classe
+	 * 
+	 * @return retorna uma string contendo as informações da classe
+	 */
+	public String toString() {
+		return "Nome: " + this.nome + "\nPontuação: " + this.pontuacao
+				+ "\nRankig: " + this.raking;
+	}
+}
