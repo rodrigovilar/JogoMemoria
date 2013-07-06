723f2119ea0b6a507c82380a6edd89c00060ac83
Lilian <lilian.thais@dce.ufpb.br>
2013-07-06 16:47:45 -0300

    Commit - Removendo comentários desnecessários.

--- src/br/com/jogo/entidades/Jogador.java
+++ src/br/com/jogo/entidades/Jogador.java
@@ -1,39 +1,28 @@
 package br.com.jogo.entidades;
 
 public class Jogador {
-	// atributos
+	
 	private String nome;
 	private int pontuacao;
 	private int raking;
 
-	// construtor
+	
 	public Jogador() {
 		this("", 0, 0);
 	}
-
-	// construtor
-	/**
-	 * Construtor da classe jogador que recebe nome e pontuação
-	 * 
-	 * @param nome
-	 *            recebe o nome do jogador
-	 * @param pontuacao
-	 *            recebe a pontuação do jogador
-	 * @param ranking
-	 *            recebe o ranking do jogador
-	 */
+	
 	public Jogador(String nome, int pontuacao, int raking) {
 		this.nome = nome;
 		this.pontuacao = pontuacao;
 		this.raking = raking;
 	}
 
-	// get: pegar
+	
 	public String getNome() {
 		return this.nome;
 	}
 
-	// set: modificar
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
@@ -54,11 +43,7 @@ public class Jogador {
 		this.raking = raking;
 	}
 
-	/**
-	 * Serve para retornar informações da classe em forma de String
-	 * 
-	 * @return retorna uma string contendo as informações da classe
-	 */
+	
 	public String toString() {
 		return "Nome: " + this.nome + "\nPontuação: " + this.pontuacao
 				+ "\nRankig: " + this.raking;
