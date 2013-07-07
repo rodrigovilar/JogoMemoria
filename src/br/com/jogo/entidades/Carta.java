888d1ad3a760a748c15cb061697dc2a4729f1de8
Emilayne <emilayne.corlett@ufpb.br>
2013-07-07 17:54:59 -0300

    Implementação da classe Carta

---
+++ src/br/com/jogo/entidades/Carta.java
@@ -0,0 +1,53 @@
+package br.com.jogo.entidades;
+
+public class Carta {
+	private int codigo;
+	private boolean virada = true;
+	private String historia;
+
+	Carta(int codigo, String historia) {
+		this.codigo = codigo;
+		this.historia = historia;
+	}
+
+	public boolean equals(Carta c) {
+		if (this.historia.equals(c.historia)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void desvira() {
+		this.virada = false;
+	}
+
+	public void vira() {
+		this.virada = true;
+	}
+
+	public boolean estaVirada() {
+		return this.virada;
+	}
+
+	public int getCodigo() {
+		return this.codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getHistoria() {
+		return this.historia;
+	}
+
+	public void setHistoria(String historia) {
+		this.historia = historia;
+	}
+
+	public String toString() {
+		return "Codigo:" + this.codigo + "\n historia";
+	}
+	
+}
