07ffa15bc257f2e38f62cb8cbc0855945f4c5218
Emilayne <emilayne.corlett@ufpb.br>
2013-07-08 16:45:28 -0300

    Refazendo pacote teste. Tentativa de tirar duplicação do pacote.

---
+++ src/br/com/jogo/teste/TesteFachada.java
@@ -0,0 +1,14 @@
+package br.com.jogo.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteFachada {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
